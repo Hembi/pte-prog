fea5918934fd225a3e26088ba3d635a3330f051f
hembi <hembi7@gmail.com>
2018-10-11 17:25:55 +0200

    leapyear-update

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 
 namespace leapyear
 {
@@ -6,15 +8,39 @@ namespace leapyear
     {
         static void Main(string[] args)
         {
-            string textToRead = Console.ReadLine();
-            int number;
-            if(Int32.TryParse(textToRead, out number))
+            List<int> imputYears = new List<int>();
+            
+            for(int i = 0; i < 2; i++)
             {
-                if(isLeapYear(number))
-                    Console.Write("Ez bizony egy szökőév volt");
-                else
-                    Console.Write("Nem szökőév.");
+                int number;
+                string textToRead;
+                do
+                {
+                    Console.Write("Adj meg egy évszámot:\n");
+                    textToRead = Console.ReadLine();
+                }
+                while(!Int32.TryParse(textToRead, out number));
+                imputYears.Add(number);
             }
+            
+            List<int> betweenYears = new List<int>(); //Tömbbe szedve a feladat kérése alapján
+            for( int i = imputYears[0]; i < imputYears[1]; i++ )
+            {
+                if(isLeapYear(i))
+                    betweenYears.Add(i);
+            }
+            Console.Write("Szökőévek {0} és {1} között:\n", imputYears[0], imputYears[1]);
+
+            if(betweenYears.Count > 0)
+            for( int i = 0; i < betweenYears.Count; i++ )
+            {
+                if(i > 0)
+                    Console.Write(", ");
+                
+                Console.Write("{0}", betweenYears[i]);
+            }
+            else
+                Console.Write("Nem volt egy szökőév sem a kettő megadott év között.");
         }
         static bool isLeapYear(int year)
         {
